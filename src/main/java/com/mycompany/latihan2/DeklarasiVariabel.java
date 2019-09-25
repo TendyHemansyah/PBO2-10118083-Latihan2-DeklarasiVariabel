18efa6305660b9f7c6133d74e8125e30805296eb
Tendy Hermansyah <55569231+TendyHemansyah@users.noreply.github.com>
2019-09-25 13:54:06 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan2/DeklarasiVariabel.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihan2;
+
+/**
+ *
+ * @author Tendy
+ * NAMA     : Tendy Hermansyah
+ * KELAS    : PBO2
+ * NIM      : 10118083
+ * Deskripsi Program : Deklarasi dan Pengaksesan Variabel
+ */
+public class DeklarasiVariabel {
+    public static void main(String[] args){
+        
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHI =3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan Hasil
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi nilai PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+
+    }
+}
